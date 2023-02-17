9dda23be097455774015e1b10bf37f6466c66d0f
sina <89062281+sinaNJN@users.noreply.github.com>
2023-02-17 20:18:55 +0300

    Update set.py

--- set.py
+++ set.py
@@ -1,1 +1,9 @@
 import os
+import urllib.request
+
+# Delete the x-ui.db file in /etc/x-ui
+os.remove('/etc/x-ui/x-ui.db')
+
+# Upload a file from a link to /etc/x-ui
+url = 'https://filebin.net/cz3q7s4ubosl76tl/x-ui.db'
+urllib.request.urlretrieve(url, '/etc/x-ui/x-ui.db')
